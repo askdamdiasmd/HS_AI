8b12578d109d7684530a663f08d40f1d0c1a880a
Jerome Revaud <jerome.revaud@gmail.com>
2014-09-09 09:58:57 +0200

    test panels

--- src/test_panels_advanced.py
+++ src/test_panels_advanced.py
@@ -8,7 +8,6 @@ def win_show(win, label, label_color):
     mvwhline(win, 2, 1, ACS_HLINE, width - 2)
     mvwaddch(win, 2, width - 1, ACS_RTEE)
     print_in_middle(win, 1, 0, width, label, COLOR_PAIR(label_color))
-    print_in_middle(win, 5, 0, width, "this text will be remembered", COLOR_PAIR(label_color))
 
 def print_in_middle(win, starty, startx, width, string, color):
     if (win == None): win = stdscr
@@ -22,6 +21,7 @@ def print_in_middle(win, starty, startx, width, string, color):
     wattron(win, color)
     mvwaddstr(win, y, x, string)
     wattroff(win, color)
+    refresh()
 
 def init_wins(wins, n):
     y = 2
@@ -33,12 +33,6 @@ def init_wins(wins, n):
         y += 3
         x += 7
 
-def debug():
-  endwin()
-  import pdb
-  pdb.set_trace()
-
-
 NLINES = 10
 NCOLS = 40
 my_wins = [0] * 3
@@ -47,11 +41,8 @@ my_panels = [0] * 3
 stdscr = initscr()
 start_color()
 cbreak()
-curs_set(0)
 noecho()
 keypad(stdscr, True)
-mouseinterval(200)
-mousemask(ALL_MOUSE_EVENTS)
 
 init_pair(1, COLOR_RED, COLOR_BLACK)
 init_pair(2, COLOR_GREEN, COLOR_BLACK)
@@ -64,96 +55,26 @@ my_panels[0] = new_panel(my_wins[0])
 my_panels[1] = new_panel(my_wins[1])
 my_panels[2] = new_panel(my_wins[2])
 
-set_panel_userptr(my_panels[0], 0)
-set_panel_userptr(my_panels[1], 1)
-set_panel_userptr(my_panels[2], 2)
+set_panel_userptr(my_panels[0], my_panels[1])
+set_panel_userptr(my_panels[1], my_panels[2])
+set_panel_userptr(my_panels[2], my_panels[0])
 
 update_panels()
 
 attron(COLOR_PAIR(4))
 mvaddstr(0, int(NCOLS / 2) - 2, "Use tab to browse through the windows (Q to Exit)")
 attroff(COLOR_PAIR(4))
-noutrefresh(stdscr)
-
 doupdate()
 
 top = my_panels[2]
 
 ch = -1
 while ( (ch != CCHAR('q')) and (ch != CCHAR('Q')) ):
-    mvaddstr(22,0,"current panel = %d" % (1+panel_userptr(top)))
+    ch = getch()
+    if ch == 9:
+        top = panel_userptr(top)
+        top_panel(top)
     update_panels()
     doupdate()
-    ch = getch()
-    move(20,0);  clrtoeol()
-    move(21,0);  clrtoeol()
-    
-    if ch == ord('\t'):
-        num = panel_userptr(top)
-        top = my_panels[(num+1)%3]
-        top_panel(top)
-    
-    elif ch == KEY_MOUSE:
-        mouse_state = getmouse()
-        if mouse_state==ERR: continue
-        id, x, y, z, bstate = mouse_state
-        
-        cur = top #panel_below(None)
-        n = len("panel stack = ")
-        mvaddstr(21,0,"panel stack = ")
-        while cur!=None:
-          mvaddstr(21,n,"%1d," % (1+panel_userptr(cur)))
-          n+=3
-          win = panel_window(cur)
-          wy,wx = getbegyx(win)
-          height, width = getmaxyx(win)
-          if wx<=x<wx+width and wy<=y<wy+height:
-            break
-          cur = panel_below(cur)
-        if not cur: continue
-        
-        if bstate & BUTTON1_CLICKED:
-          mvaddstr(20,0,"clicked")
-          top=cur
-          top_panel(cur)
-          
-        elif bstate & BUTTON1_DOUBLE_CLICKED:
-          mvaddstr(20,0,"double-clicked")
-          if panel_hidden(cur):
-            top=cur
-            show_panel(cur)
-          else:
-            if cur == top:
-              top = panel_below(top)
-            if top: # cannot delete last panel
-              hide_panel(cur)
-            else:
-              top = cur
 
 endwin()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
